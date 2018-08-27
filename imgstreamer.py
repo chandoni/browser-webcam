02f6885b154c9c02098873a8e4cd1236674c187d
chandoni <42420414+chandoni@users.noreply.github.com>
2018-08-27 13:10:49 -0700

    Update imgstreamer.py

--- imgstreamer.py
+++ imgstreamer.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-import numpy as np
 import cv2
 import os
 import sys
@@ -7,50 +5,35 @@ import asyncio
 import websockets
 import threading
 
+cam = cv2.VideoCapture(0)
+
 def readimgsize(im_filename):
     f = open(im_filename, 'rb') # read bytes...
     f.seek(0, os.SEEK_END) # (offset[, whence])
     fileSize = f.tell() # return position of the read pointer within the file
-    print(fileSize)
+    #print(fileSize)
     f.seek(0, os.SEEK_SET)
     buffer = f.read(fileSize) # read fileSize number of bytes into buffer
     return buffer
 
-def recognizeFace(im):
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        im = cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = im[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-    cv2.imwrite('img-cap.jpg', im, [int(cv2.IMWRITE_JPEG_QUALITY), 60])
-
-
-###################################################################################################3
-
-face_cascade = cv2.CascadeClassifier('/usr/local/lib/python3.5/dist-packages/cv2/data/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('/usr/local/lib/python3.5/dist-packages/cv2/data/haarcascade_eye.xml')
-rec_face = True
-cam = cv2.VideoCapture(0)
-
 async def sendimage(websocket, path):
-    fps = 30
+    fps = 80
+    #idx = 0    this was also for version 1
     while True:
         retval, frame = cam.read()
         if retval != True:
             raise ValueError("Can't read frame")
 
-        cv2.imwrite('img-cap.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 60]) # write image and set quality
-
-        if (rec_face == True):
-            img = cv2.imread('img-cap.jpg')
-            recognizeFace(img)
-
+        cv2.imwrite('img-cap.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 40]) # write image and set quality
         imgbytes = readimgsize('img-cap.jpg') # read the newly rewritten image into bytes
+
+        # this section was for version 1 and only swapped between two images on the webpage
+        #if idx == 0:
+        #    imgbytes = readimgsize('cracked300x300.jpg')
+        #    idx = 1
+        #else:
+        #    imgbytes = readimgsize('moby300x300.jpg')
+        #    idx = 0
 
         await websocket.send(imgbytes) # send the bytes of the newly captured image
         await asyncio.sleep(1 / fps)
